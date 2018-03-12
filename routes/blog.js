b6ef12c195fc94300d5d3e8811d95069bbd19f62
Merizza Khan <merizza.azwir@gmail.com>
2018-03-12 17:11:13 +0000

    Added add post page

--- routes/blog.js
+++ routes/blog.js
@@ -1,9 +1,14 @@
 var express = require("express");
 var router = express.Router();
 
-/*GET about page*/
+/*GET blog page*/
 router.get("/", function(req, res, next) {
   res.render("partials/blog");
+});
+
+/*GET addpost page*/
+router.get("/add", function(req, res, next) {
+  res.render("partials/addpost");
 });
 
 module.exports = router;
