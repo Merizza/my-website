c8b63e190f9e47a5c56b173ffdf9d72db2b814fe
Merizza Khan <merizza.azwir@gmail.com>
2018-03-16 17:43:58 +0000

    Fix blog section display on the homepage

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,20 @@ var express = require("express");
 var router = express.Router();
 
 router.get("/", function (req, res) {
-    res.render("index");
+  /*Connect to the database*/
+  var mongoose = require("mongoose");
+  mongoose.connect("mongodb://localhost/myblog");
+  var db = mongoose.connection;
+
+  var {Post} = require("../db/models/post.js");
+  
+  Post.find({}, function(err, post) {
+    if(err) throw err;
+    
+     res.render("index", {
+        "post": post
+     });
+  });
 });
 
 module.exports = router;
